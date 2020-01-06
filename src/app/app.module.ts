726728a0bfad09c457329aa11c91d973c38bae94
parthsunilpatil <parth.patil@imaginea.com>
2020-01-06 14:53:24 +0530

    Angular Microfrontend Demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactCreateComponent,
+    ContactListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
+  entryComponents: [
+    AppComponent,
+    HomeComponent,
+    ContactCreateComponent,
+    ContactListComponent,
+    HeaderComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+  ngDoBootstrap() {
+    const myCustomElement = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('app-contacts-bootstrap-demo', myCustomElement);
+  }
+}
